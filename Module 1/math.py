8f6399acc710e514f9c6fe47de8fe3094b67ac42
afrinasultana43 <afrina734374@gmail.com>
2026-09-20 21:52:23 +0600

    add math operations for ceil, floor, and round

---
+++ Module 1/math.py
@@ -0,0 +1,15 @@
+import math
+#ceil
+x =4.5
+y =4.1
+print(math.ceil(x))
+print(math.ceil(y))
+#floor
+a =3.5
+b =0.002
+print(math.floor(a))
+print(math.floor(b))
+
+#round
+txt =610.60
+print(round(txt))
